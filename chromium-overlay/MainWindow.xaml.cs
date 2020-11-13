0ac3d0564a765064a5bb755b4315ac86ef5858cd
MogsFriend <antares@ffxiv.me>
2020-11-13 19:48:25 +0900

    update

---
+++ chromium-overlay/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace chromium_overlay
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var settings = new CefSettings();
+            settings.CefCommandLineArgs.Add("enable-gpu", "1");
+            settings.CefCommandLineArgs.Add("enable-webgl", "1");
+            settings.CefCommandLineArgs.Add("enable-begin-frame-scheduling", "1");
+            settings.CefCommandLineArgs.Add("--off-screen-frame-rate", "60");
+            settings.WindowlessRenderingEnabled = true;
+            Cef.Initialize(settings);
+            InitializeComponent();
+
+            WPFCEF.IsBrowserInitializedChanged += (s, e) =>
+            {
+                if (!WPFCEF.IsBrowserInitialized) return;
+                WPFCEF.ShowDevTools();
+            };
+        }
+    }
+}
